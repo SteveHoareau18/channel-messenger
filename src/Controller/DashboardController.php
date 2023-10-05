624263341b072ab4520982caee04a4547079b7fb
kotou-boina <kotou.boina@gmail.com>
2023-10-05 13:47:51 +0200

    protect dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,21 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
 class DashboardController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_dashboard')]
+    #[Route('/', name: 'app_dashboard')]
     public function index(): Response
     {
-        if(!$this->getUser()) return $this->redirectToRoute('auth_login');
-        return $this->render('index.html.twig', [
-
-        ]);
-    }
-
-    #[Route('/', name: 'app_root')]
-    public function root(): Response
-    {
-        return $this->redirectToRoute('app_dashboard');
+        return $this->render('index.html.twig', []);
     }
 }
