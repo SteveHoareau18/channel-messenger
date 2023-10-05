7b530e2bc5b1888bc27209f63470e152e76baaaa
SteveHoareau18 <steve.hoareau1@gmail.com>
2023-10-05 16:06:32 +0400

    remove root route (unnecessary)

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,18 +8,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_dashboard')]
+    #[Route('/', name: 'app_dashboard')]
     public function index(): Response
     {
         if(!$this->getUser()) return $this->redirectToRoute('auth_login');
         return $this->render('index.html.twig', [
 
         ]);
-    }
-
-    #[Route('/', name: 'app_root')]
-    public function root(): Response
-    {
-        return $this->redirectToRoute('app_dashboard');
     }
 }
