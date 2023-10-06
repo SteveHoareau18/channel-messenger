6598bf7a5b09cac79abedc0a1e6562f3aab85141
SteveHoareau18 <steve.hoareau1@gmail.com>
2023-10-06 11:32:43 +0400

    update ui et fix ui

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\ChannelUser;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
         if(!$this->getUser()) return $this->redirectToRoute('auth_login');
+        $other_channels = $entityManager->getRepository(ChannelUser::class)->findBy(['user'=>$this->getUser()]);
         return $this->render('index.html.twig', [
-
+            'other_channels'=>$other_channels
         ]);
     }
 }
