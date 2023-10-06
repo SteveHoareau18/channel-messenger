f3d2e274376d0dde5e570a4a99a04d89b7f953a2
SteveHoareau18 <steve.hoareau1@gmail.com>
2023-10-06 14:31:52 +0400

    generate api for ajax

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\ChannelUser;
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,5 +22,21 @@ class DashboardController extends AbstractController
         return $this->render('index.html.twig', [
             'other_channels'=>$other_channels
         ]);
+    }
+
+    #[Route('/api/filter/user-list', name: 'api_filter_user_list',methods: ['GET'])]
+    public function apiFilterUserList(Request $request, UserRepository $userRepository): JsonResponse{
+        if(!$request->query->has('query') || $request->query->get('query') == "") return new JsonResponse(array());
+        $query = $request->query->get('query');
+        $usernames = $userRepository->findByWithUsernameLike($query);
+        $response = array();
+        foreach ($usernames as $username){
+            array_push($response,$username->getUsername());
+        }
+//        $emails =  $userRepository->findByWithEmailLike($query);
+//        if($emails != null){
+//            array_push($response,$emails);
+//        }
+        return new JsonResponse($response);
     }
 }
